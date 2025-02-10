d3493a012cb5a16f237c289631459f204233a024
beyoungjae <pz1998@naver.com>
2025-02-10 17:22:56 +0900

    home update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import CssBaseline from '@mui/material/CssBaseline'
+import { styled } from '@mui/material/styles'
+import { Box } from '@mui/material'
 import Navbar from './components/shared/Navbar'
+import Home from './components/Home'
+import Footer from './components/shared/Footer'
+
+// 네비바 아래 컨텐츠를 위한 컨테이너
+const MainContent = styled(Box)(({ theme }) => ({
+   paddingTop: '124px',
+   [theme.breakpoints.down('md')]: {
+      paddingTop: '55px',
+   },
+}))
 
 function App() {
    return (
       <>
          <CssBaseline />
          <Navbar />
+         <MainContent>
+            <Home />
+         </MainContent>
+         <Footer />
       </>
    )
 }
