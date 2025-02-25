4cc54b5517c15c94c29445e3a4d4138d8ed8d3c6
limeunha <eh1501@naver.com>
2025-02-25 18:32:36 +0900

    고객센터

--- src/components/customer/Board.jsx
+++ src/components/customer/Board.jsx
@@ -1,4 +1,101 @@
-//notice, qna
-const Board = () => {}
+import React, { useState } from 'react'
+import { Box, Typography, Pagination } from '@mui/material'
+import { styled } from '@mui/material/styles'
+
+// 보드 컨테이너
+const BoardContainer = styled(Box)(({ theme }) => ({
+   [theme.breakpoints.down('lg')]: {
+      fontSize: '1.3rem',
+   },
+   [theme.breakpoints.down('md')]: {
+      fontSize: '1.2rem',
+   },
+   [theme.breakpoints.down('sm')]: {
+      fontSize: '1rem',
+   },
+}))
+
+// 보드 아이템 컨테이너
+const BoardItemContainer = styled(Box)(({ theme }) => ({
+   borderBottom: `1px solid ${theme.palette.divider}`,
+}))
+
+// 콘텐츠 박스
+const ContentBox = styled(Box)(({ theme, show }) => ({
+   display: show ? 'block' : 'none',
+   padding: '20px',
+   backgroundColor: '#FAFAFA',
+}))
+
+// 보드 타이틀
+const BoardTitle = styled(Box)(({ theme }) => ({
+   fontWeight: 'bold',
+   cursor: 'pointer',
+   paddingTop: '20px',
+   paddingLeft: '20px',
+   paddingBottom: '30px',
+   alignItems: 'center',
+   [theme.breakpoints.down('lg')]: {
+      fontSize: '1.1rem',
+   },
+   [theme.breakpoints.down('md')]: {
+      fontSize: '1rem',
+   },
+   [theme.breakpoints.down('sm')]: {
+      fontSize: '0.9rem',
+   },
+}))
+
+// 페이지네이션 스타일
+const PaginationContent = styled(Box)(({ theme }) => ({
+   display: 'flex',
+   paddingTop: '50px',
+   justifyContent: 'center',
+   alignItems: 'center',
+   textAlign: 'center',
+   [theme.breakpoints.down('sm')]: {
+      paddingTop: '30px',
+   },
+}))
+
+const Board = ({ result }) => {
+   const [openItemId, setOpenItemId] = useState(null)
+   const [currentPage, setCurrentPage] = useState(1)
+   const itemsPerPage = 5
+
+   const handleTitleClick = (id) => {
+      setOpenItemId((prevId) => (prevId === id ? null : id))
+   }
+
+   const handlePageChange = (event, value) => {
+      setCurrentPage(value)
+   }
+
+   const startIndex = (currentPage - 1) * itemsPerPage
+   const endIndex = startIndex + itemsPerPage
+   const currentItems = result.slice(startIndex, endIndex)
+
+   return (
+      <BoardContainer>
+         {currentItems.map((item) => (
+            <BoardItemContainer key={item.id}>
+               <BoardTitle>
+                  <Typography variant="h6" onClick={() => handleTitleClick(item.id)} sx={{ fontSize: '15px' }}>
+                     {item.title}
+                  </Typography>
+               </BoardTitle>
+               <ContentBox show={openItemId === item.id}>
+                  <Typography variant="body2">{item.content} </Typography>
+               </ContentBox>
+            </BoardItemContainer>
+         ))}
+
+         {/* 페이지네이션 */}
+         <PaginationContent>
+            <Pagination count={Math.ceil(result.length / itemsPerPage)} page={currentPage} onChange={handlePageChange} shape="rounded" color="#EEEEEE" />
+         </PaginationContent>
+      </BoardContainer>
+   )
+}
 
 export default Board
--- src/components/customer/Top4Card.jsx
+++ src/components/customer/Top4Card.jsx
@@ -1,43 +1,53 @@
-import { Box, Typography, Card as MuiCard, CardContent } from '@mui/material'
+import { Box, Typography, Card as MuiCard, CardContent, Button } from '@mui/material'
 import { styled } from '@mui/material/styles'
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight'
+import { Link } from 'react-router-dom'
+
+// 카드 페이지 컨테이너
+const Top4CardContainer = styled(Box)(({ theme }) => ({
+   width: '100%',
+   backgroundColor: theme.palette.background.default,
+}))
 
 // 카드 컨테이너
-const StyledCardBox = styled(Box)(({ theme }) => ({
-   display: 'flex',
-   mx: theme.spacing(2),
-   my: theme.spacing(1),
-}))
+const StyledCardBox = styled(Box)({
+   display: 'grid',
+   justifyContent: 'center',
+   gridTemplateRows: '20px auto 60px', // top(10px), content(auto), bottom(60px)
+})
 
 // 카드 그리드
 const CardGrid = styled(Box)(({ theme }) => ({
    display: 'grid',
    gridTemplateColumns: 'repeat(4, 1fr)',
-   padding: '50px',
-   gap: '32px',
-   maxWidth: '1200px',
-   margin: '0 auto',
-   [theme.bps.md]: {
+   padding: '30px 0 50px 0',
+   gap: '2vw', // 화면 크기에 따라 갭 자동 변경(뷰포트 너비를 기준으로 gap자동 조정)
+   [theme.breakpoints.down('md')]: {
       gridTemplateColumns: 'repeat(2, 1fr)',
-      gap: '24px',
    },
-   [theme.bps.sm]: {
+   [theme.breakpoints.down('sm')]: {
       gridTemplateColumns: '1fr',
-      gap: '16px',
    },
 }))
 
 // 카드
 const Card = styled(MuiCard)(({ theme }) => ({
-   width: '215px',
+   width: '221px',
    height: '125px',
    borderRadius: theme.shape.borderRadius.medium,
-   border: `1px solid #AFAFAF`,
+   border: '1px solid #AFAFAF',
    borderRadius: '20px',
    boxShadow: 'none',
+   [theme.breakpoints.down('md')]: {
+      width: '250px',
+   },
+   [theme.breakpoints.down('sm')]: {
+      width: '290px',
+   },
 }))
 
 // 카드 타이틀
-const Cardtitle = styled(Typography)(({ theme }) => ({
+const Cardtitle = styled(Typography)(() => ({
    fontSize: '15px',
    fontWeight: 'bold',
    textAlign: 'center',
@@ -45,8 +55,23 @@ const Cardtitle = styled(Typography)(({ theme }) => ({
    justifyContent: 'center',
    alignItems: 'center',
    height: '100%',
-   // whiteSpace: 'normal', // 줄 바꿈
    overflow: 'hidden',
+}))
+
+// 전체보기 링크
+const ViewLink = styled(Typography)(({ theme }) => ({
+   display: 'flex',
+   justifyContent: 'flex-end',
+   color: theme.palette.text.primary,
+   textDecoration: 'none',
+}))
+
+// 버튼 스타일
+const CustomerButton = styled(Button)(({ theme }) => ({
+   backgroundColor: theme.palette.primary.contrastText,
+   padding: '6px 12px',
+   border: '1px solid #D3D3D3',
+   fontSize: '0.8rem',
 }))
 
 // 더미 데이터
@@ -54,17 +79,32 @@ const dummyCards = [{ title: 'Q. 로그인이 안될 경우 해결방법 ' }, { 
 
 const Top4Card = () => {
    return (
-      <StyledCardBox>
-         <CardGrid>
-            {dummyCards.map((card, index) => (
-               <Card key={index}>
-                  <CardContent>
-                     <Cardtitle variant="h6">{card.title}</Cardtitle>
-                  </CardContent>
-               </Card>
-            ))}
-         </CardGrid>
-      </StyledCardBox>
+      <Top4CardContainer>
+         <StyledCardBox>
+            <Box>
+               <ViewLink component={Link} to="/">
+                  전체보기 <KeyboardArrowRightIcon />
+               </ViewLink>
+            </Box>
+            <CardGrid>
+               {dummyCards.map((card, index) => (
+                  <Card key={index}>
+                     <CardContent>
+                        <Cardtitle variant="h6">{card.title}</Cardtitle>
+                     </CardContent>
+                  </Card>
+               ))}
+            </CardGrid>
+            <Box>
+               <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
+                  <Typography sx={{ marginRight: '8px', fontSize: '0.9rem' }}>더 자세한 질문은</Typography>
+                  <Link to="/">
+                     <CustomerButton>1:1문의하기</CustomerButton>
+                  </Link>
+               </Box>
+            </Box>
+         </StyledCardBox>
+      </Top4CardContainer>
    )
 }
 
